661f67bd40bb0140bb7f343755794939a5e491ac
thomasfinch <tomf64@gmail.com>
2016-10-17 18:47:29 -0400

    Changed key events to handle key down and up separately

--- DigitalInstrument.rb
+++ DigitalInstrument.rb
@@ -1,7 +1,24 @@
 Shoes.app do
-	@info = para "No key is pressed."
-	keypress do |k|
-		@info.replace "#{k} was pressed."
-		debug(k)
+	keydown do |k|
+		# Start playing piano key sounds here
+		case k
+		when 'a'
+			para "you pressed a down\n"
+		when 's'
+			para "you pressed s down\n"
+		end
+		
+		para "#{k.inspect} down\n"
+	end
+	keyup do |k|
+		# Stop playing piano key sounds here
+		case k
+		when 'a'
+			para "you pressed a up\n"
+		when 's'
+			para "you pressed s up\n"
+		end
+		
+		para "#{k.inspect} up\n"
 	end
 end
