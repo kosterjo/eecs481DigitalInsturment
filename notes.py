e128dd2b91f05340506caa4b9c0469a565db270a
bliminate <blimmer@umich.edu>
2016-11-07 15:04:18 -0500

    Create sound class to play notes, and experimenting with playing notes

--- notes.py
+++ notes.py
@@ -6,11 +6,19 @@ import time
 import sys
 
 fluidsynth.init("HS Synth Collection I.sf2", "alsa")
+n = Note("C-5")
+n.channel = 5
+n.velocity = 100
+fluidsynth.play_Note(60)
 fluidsynth.play_Note(Note("C-5"))
+#fluidsynth.play_Note(Note("E-5"))
+#fluidsynth.play_Note(Note("G-5"))
 time.sleep(5)
-fluidsynth.stop_Note(Note("C-5"), 1)
+fluidsynth.stop_Note(1, 1)
+#fluidsynth.stop_Note(Note("E-5"), 1)
+#fluidsynth.stop_Note(Note("G-5"), 1)
 
-fluidsynth.play_Note(chords.major_triad("C"))
-time.sleep(5)
-fluidsynth.stop_Note(chords.major_triad("C"), 1)
+#fluidsynth.play_NoteContainer(ch.major_triad("C"))
+#time.sleep(5)
+#fluidsynth.stop_NoteContainer(ch.major_triad("C"), 1)
 
